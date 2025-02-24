35925c10526377e38568496b4c8a7e98b0d702b2
Quoc Huy <thanhquay113@gmail.com>
2025-02-24 11:57:06 +0700

    chapter15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -44,10 +46,15 @@ export async function createInvoice(prevState: State, formData: FormData) {
       message: "Missing Fields. Failed to Create Invoice.",
     };
   }
+  // const rawFormData = {
+  //   customerId: formData.get("customerId"),
+  //   amount: formData.get("amount"),
+  //   status: formData.get("status"),
+  // };
+  // const { customerId, amount, status } = validatedFields.data;
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
@@ -70,17 +77,33 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
   await sql`
-      UPDATE invoices
+        UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
-  `;
-
+    `;
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
